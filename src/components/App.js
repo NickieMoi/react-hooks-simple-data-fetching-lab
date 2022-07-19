1f49fa9623051b34da8d309dfca3d1c2f8bb501f
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-07-19 10:47:37 +0300

    commit tree 7172eb1576d5e5008fa1de7f80514f07a4cd497a

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import { useState, useEffect } from "react";
+
+function App(){
+  const [dog, setdog] = useState([])
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then( res => res.json())
+    .then( res => {
+      setdog(res)
+      setIsLoaded(true)
+    });
+  }, []);
+
+  if(!isLoaded) return <p>Loading...</p>;
+  // console.log(dog)
+
+  return (
+    <img src={dog.message} alt = "A Random Dog" />
+  )
+}
+
+
+export default App;
